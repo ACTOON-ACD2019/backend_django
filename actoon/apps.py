d1c367e729d7e3a3e032855591d0b5258703198f
Dongkyun Yoo <k3nuku@gmail.com>
2019-11-25 06:36:01 +0900

    [Add] dl wrapper - rpc producer
    Signed-off-by: Dongkyun Yoo <k3nuku@gmail.com>

--- actoon/apps.py
+++ actoon/apps.py
@@ -1,5 +1,89 @@
 from django.apps import AppConfig
+import uuid
+import json
+from aio_pika import connect, IncomingMessage, Message
+from django.core.files import File
+import base64
+import asyncio
+import zipfile
+import os
 
 
 class ActoonConfig(AppConfig):
     name = 'actoon'
+
+
+class RpcClient:
+    def __init__(self, loop):
+        self.connection = None
+        self.channel = None
+        self.callback_queue = None
+        self.futures = {}
+        self.loop = loop
+
+    async def connect(self):
+        self.connection = await connect(
+            "amqp://test:test1234@beta.actoon.sokdak.me/", loop=self.loop
+        )
+        self.channel = await self.connection.channel()
+        self.callback_queue = await self.channel.declare_queue(
+            exclusive=True
+        )
+        await self.callback_queue.consume(self.on_response)
+
+        return self
+
+    def on_response(self, message: IncomingMessage):
+        future = self.futures.pop(message.correlation_id)
+        future.set_result(message.body)
+
+    async def call_cut_slicing(self, filename):
+        hexa = open(filename.file.name, 'rb').read()
+        correlation_id = str(uuid.uuid4())
+        future = self.loop.create_future()
+
+        self.futures[correlation_id] = future
+
+        await self.channel.default_exchange.publish(
+            Message(
+                base64.b64encode(hexa),
+                content_type=filename.file.name.split('.')[-1],
+                correlation_id=correlation_id,
+                reply_to=self.callback_queue.name,
+            ),
+            routing_key='rpc_cut_slicing_queue',
+        )
+
+        return await future
+
+
+async def request(loop, task, filename=None):
+    deeplearning_client = await RpcClient(loop).connect()
+    print(" [x] Requesting cut slicing %s" % filename)
+
+    if task is 'cut_slicing':
+        # make a rpc call
+        response = await deeplearning_client.call_cut_slicing(filename)
+        open('/tmp/result_received.zip', 'wb').write(base64.b64decode(response))
+
+        # uncompressing zip
+        with zipfile.ZipFile('/tmp/result_received.zip', 'r') as zip_ref:
+            zip_ref.extractall('/tmp/actoon')
+
+        # save files into database
+        for root, dirs, files in os.walk('/tmp/actoon/'):
+            if root.__contains__('bubble'):
+                for file in files:
+                    value = open(os.path.join(root, file), 'rb')
+                    pass
+            elif root.__contains__('cut'):
+                pass
+            elif root.__contains__('text'):
+                pass
+
+        # return object
+    else:
+        # preprocessing histories
+
+        # go ahead
+        pass
