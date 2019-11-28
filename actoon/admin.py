7733283200cb6f7ce8d5e59dabbb15a006512fec
Dongkyun Yoo <k3nuku@gmail.com>
2019-11-28 21:36:46 +0900

    [Add] models into django-admin
    Signed-off-by: Dongkyun Yoo <k3nuku@gmail.com>

--- actoon/admin.py
+++ actoon/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
+from actoon.models import Effect, Media, Cut
 
 # Register your models here.
+admin.site.register(Effect)
+admin.site.register(Media)
+admin.site.register(Cut)
