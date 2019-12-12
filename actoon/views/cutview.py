706bcddd07db05343f64dbfba6dedb592bcf813d
Dongkyun Yoo <k3nuku@gmail.com>
2019-12-13 05:31:33 +0900

    [Add] csrf_exempt decorator for update function in cutview
    Signed-off-by: Dongkyun Yoo <k3nuku@gmail.com>

--- actoon/views/cutview.py
+++ actoon/views/cutview.py
@@ -1,6 +1,8 @@
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.shortcuts import get_list_or_404, get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.utils import json
@@ -79,6 +81,7 @@ class CutView(viewsets.ModelViewSet):
 
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+    @method_decorator(csrf_exempt)
     def update(self, request, pk=None, cpk=None):
         project_instance = get_project(self.request.user, pk)
         media_instance = get_media(project_instance)
